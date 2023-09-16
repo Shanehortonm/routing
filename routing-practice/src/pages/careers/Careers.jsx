cd23dd08889b26f4b61068f501034ed5c9e70d37
Shanehortonm <shanehortonm@gmail.com>
2023-09-15 21:53:31 -0400

    added careers page

--- routing-practice/src/pages/careers/Careers.jsx
+++ routing-practice/src/pages/careers/Careers.jsx
@@ -1,6 +1,23 @@
-// const careersLoader = async ()
-//  => {}
+import { useLoaderData, Link } from "react-router-dom";
 
 export const Careers = () => {
-  return <div className="careers"></div>;
+  const careerData = useLoaderData();
+
+  return (
+    <div className="careers">
+      {careerData.map((career) => (
+        <Link to="/" key={career.id}>
+          <p>{career.title}</p>
+          <p>Based in {career.location}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+// Loader Function
+export const careersLoader = async () => {
+  const res = await fetch("http://localhost:4000/careers");
+
+  return res.json();
 };
